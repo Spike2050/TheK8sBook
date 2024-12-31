d759dfb378925f4f11e7c25e9618d08aee0d3844
Spike2050 <spike2050@gmail.com>
2024-12-31 21:17:14 +0100

    Dasboard + Ingress Nginx

---
+++ kubernetes-dashboard/install_dashboard.ps1
@@ -0,0 +1,3 @@
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
